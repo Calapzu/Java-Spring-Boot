7cc33a06b22d1c7a279bb576a90e1bb466a3ff11
Calapzu <yamsoncalapzu@gmail.com>
2022-08-16 22:10:15 -0500

    Se hace un CRUD guiado del curso de Java Fullstack de lucas Moy

---
+++ src/main/java/com/cursojava/curso/models/Usuario.java
@@ -0,0 +1,33 @@
+package com.cursojava.curso.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+
+    @Getter @Setter @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    @Getter @Setter @Column(name = "nombre")
+    private String nombre;
+
+    @Getter @Setter @Column(name = "apellido")
+    private String apellido;
+
+    @Getter @Setter @Column(name = "email")
+    private String email;
+
+    @Getter @Setter @Column(name = "telefono")
+    private String telefono;
+
+    @Getter @Setter @Column(name = "password")
+    private String password;
+
+
+}
